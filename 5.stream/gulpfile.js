e9fbe607c8ae089e61fe18e018d0ad79235724ce
yuguo <fishguo321@gmail.com>
2016-11-16 11:11:25 +0800

    add gulp-less

---
+++ 5.stream/gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var path = require('path');
+
+gulp.task('default', function(){
+	return gulp.src('./less/**/*.less')
+		.pipe(less())
+		.pipe(gulp.dest('./public/css'));
+})
